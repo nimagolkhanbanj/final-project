9f300e9621d09354c308eb0cd9df017bee05a493
nimagolkhanban <ningoban@gmail.com>
2023-09-15 07:39:33 +0330

    fix:accounts model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=25, unique=True)
+    email = models.EmailField( unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_modify = models.DateTimeField( auto_now=True)
+
+    USERNAME_FIELD = 'username'
+
+    REQUIRED_FIELDS = ["email"]
+
+    def __str__(self):
+        return self.username
