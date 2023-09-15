3312e80f0c0557e15afb127ec6187b6d19e55f5b
nimagolkhanban <ningoban@gmail.com>
2023-09-15 07:31:58 +0330

    fix:home setup

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 
 # Create your views here.
+
+class Status(APIView):
+
+    def get(self, request):
+        return Response({"message": "Server is running"}, status=status.HTTP_200_OK)
