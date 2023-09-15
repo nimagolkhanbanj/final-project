5dc2af2c88de9c5903fc88d889e67da46d93cffc
nimagolkhanban <ningoban@gmail.com>
2023-09-15 07:26:47 +0330

    model like

--- interactions/models.py
+++ interactions/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+
+
+
+
+
