59a133c9d4ed5a9a3015fdb719ae2ad33334d80b
nimagolkhanban <ningoban@gmail.com>
2023-09-15 08:23:05 +0330

    fix:accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from rest_framework.views import Response, APIView
+from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
+from django.contrib.auth.models import update_last_login
+import jwt, datetime
+from .models import User
+from .serializers import UserRegisterSerializer,UserLoginSerializer
 
 # Create your views here.
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        user_serializer = UserRegisterSerializer(data=request.data)
+        user_serializer.is_valid(raise_exception=True)
+        user_serializer.save()
+        return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        phone = request.data['phone']
+        password = request.data['password']
+
+        user = User.objects.filter(phone=phone).first()
+
+        if user is None:
+            raise AuthenticationFailed('User is not found')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Password is not correct')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret',algorithm='HS256')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt':token
+            }
+
+        update_last_login(None, user)
+        return response
